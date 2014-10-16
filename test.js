e24cf755760fce3d21ef66a76092c34fe0a453da
Sindre Sorhus <sindresorhus@gmail.com>
2014-10-16 21:01:17 +0200

    code style

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 var assert = require('assert');
 var strip = require('./strip-json-comments');
 
-describe('Test Cases', function() {
+describe('Test Cases', function () {
 	it('should strip comments', function () {
 		assert.strictEqual(strip('//comment\n{"a":"b"}'), '\n{"a":"b"}');
 		assert.strictEqual(strip('/*//comment*/{"a":"b"}'), '{"a":"b"}');
@@ -18,23 +18,25 @@ describe('Test Cases', function() {
 		assert.strictEqual(strip('{"\\"/*a":"b"}'), '{"\\"/*a":"b"}');
 	});
 
-	describe('Line endings', function() {
-		it('no comments', function() {
+	describe('Line endings', function () {
+		it('no comments', function () {
 			assert.strictEqual(strip('{"a":"b"\n}'), '{"a":"b"\n}');
 			assert.strictEqual(strip('{"a":"b"\r\n}'), '{"a":"b"\r\n}');
 		});
-		it('single line comment', function() {
+
+		it('single line comment', function () {
 			assert.strictEqual(strip('{"a":"b"//c\n}'), '{"a":"b"\n}');
 			assert.strictEqual(strip('{"a":"b"//c\r\n}'), '{"a":"b"\r\n}');
 		});
-		it('single line block comment', function() {
+
+		it('single line block comment', function () {
 			assert.strictEqual(strip('{"a":"b"/*c*/\n}'), '{"a":"b"\n}');
 			assert.strictEqual(strip('{"a":"b"/*c*/\r\n}'), '{"a":"b"\r\n}');
 		});
-		it('multi line block comment', function() {
+
+		it('multi line block comment', function () {
 			assert.strictEqual(strip('{"a":"b",/*c\nc2*/"x":"y"\n}'), '{"a":"b","x":"y"\n}');
 			assert.strictEqual(strip('{"a":"b",/*c\r\nc2*/"x":"y"\r\n}'), '{"a":"b","x":"y"\r\n}');
 		});
 	});
 });
-
