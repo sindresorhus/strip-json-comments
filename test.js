3247aafe69dd75e2386712b5503a42766427aad9
Simon Lydell <simon.lydell@gmail.com>
2014-11-04 19:32:24 +0100

    Switch to TDD test interface
    The test descriptions were already written TDD-style, but the BDD interface was
    used.

--- test.js
+++ test.js
@@ -2,8 +2,8 @@
 var assert = require('assert');
 var strip = require('./strip-json-comments');
 
-describe('Test Cases', function () {
-	it('should strip comments', function () {
+suite('Test Cases', function () {
+	test('should strip comments', function () {
 		assert.strictEqual(strip('//comment\n{"a":"b"}'), '\n{"a":"b"}');
 		assert.strictEqual(strip('/*//comment*/{"a":"b"}'), '{"a":"b"}');
 		assert.strictEqual(strip('{"a":"b"//comment\n}'), '{"a":"b"\n}');
@@ -11,30 +11,30 @@ describe('Test Cases', function () {
 		assert.strictEqual(strip('{"a"/*\n\n\ncomment\r\n*/:"b"}'), '{"a":"b"}');
 	});
 
-	it('should not strip comments inside strings', function () {
+	test('should not strip comments inside strings', function () {
 		assert.strictEqual(strip('{"a":"b//c"}'), '{"a":"b//c"}');
 		assert.strictEqual(strip('{"a":"b/*c*/"}'), '{"a":"b/*c*/"}');
 		assert.strictEqual(strip('{"/*a":"b"}'), '{"/*a":"b"}');
 		assert.strictEqual(strip('{"\\"/*a":"b"}'), '{"\\"/*a":"b"}');
 	});
 
-	describe('Line endings', function () {
-		it('no comments', function () {
+	suite('Line endings', function () {
+		test('no comments', function () {
 			assert.strictEqual(strip('{"a":"b"\n}'), '{"a":"b"\n}');
 			assert.strictEqual(strip('{"a":"b"\r\n}'), '{"a":"b"\r\n}');
 		});
 
-		it('single line comment', function () {
+		test('single line comment', function () {
 			assert.strictEqual(strip('{"a":"b"//c\n}'), '{"a":"b"\n}');
 			assert.strictEqual(strip('{"a":"b"//c\r\n}'), '{"a":"b"\r\n}');
 		});
 
-		it('single line block comment', function () {
+		test('single line block comment', function () {
 			assert.strictEqual(strip('{"a":"b"/*c*/\n}'), '{"a":"b"\n}');
 			assert.strictEqual(strip('{"a":"b"/*c*/\r\n}'), '{"a":"b"\r\n}');
 		});
 
-		it('multi line block comment', function () {
+		test('multi line block comment', function () {
 			assert.strictEqual(strip('{"a":"b",/*c\nc2*/"x":"y"\n}'), '{"a":"b","x":"y"\n}');
 			assert.strictEqual(strip('{"a":"b",/*c\r\nc2*/"x":"y"\r\n}'), '{"a":"b","x":"y"\r\n}');
 		});
