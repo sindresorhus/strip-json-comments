480c072c2595f92e7655b9f27a8dc0350f45e314
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-19 19:41:55 +0200

    Use `meow` in CLI

--- cli.js
+++ cli.js
@@ -1,15 +1,26 @@
 #!/usr/bin/env node
 'use strict';
 var fs = require('fs');
+var meow = require('meow');
 var strip = require('./strip-json-comments');
-var input = process.argv[2];
 
+var cli = meow({
+	help: [
+		'Usage',
+		'  $ strip-json-comments <input-file> > <output-file>',
+		'  $ strip-json-comments < <input-file> > <output-file>',
+		'',
+		'Example',
+		'  $ strip-json-comments package.json > package-stripped.json'
+	]
+});
+
+var input = cli.input[0];
 
 function getStdin(cb) {
 	var ret = '';
 
 	process.stdin.setEncoding('utf8');
-
 	process.stdin.on('data', function (data) {
 		ret += data;
 	});
@@ -19,23 +30,15 @@ function getStdin(cb) {
 	});
 }
 
-if (process.argv.indexOf('-h') !== -1 || process.argv.indexOf('--help') !== -1) {
-	console.log('strip-json-comments input-file > output-file');
-	console.log('or');
-	console.log('strip-json-comments < input-file > output-file');
-	return;
-}
-
-if (process.argv.indexOf('-v') !== -1 || process.argv.indexOf('--version') !== -1) {
-	console.log(require('./package').version);
-	return;
+if (!input && process.stdin.isTTY) {
+	console.error('Expected a filename');
+	process.exit(1);
 }
 
 if (input) {
 	process.stdout.write(strip(fs.readFileSync(input, 'utf8')));
-	return;
+} else {
+	getStdin(function (data) {
+		process.stdout.write(strip(data));
+	});
 }
-
-getStdin(function (data) {
-	process.stdout.write(strip(data));
-});
--- readme.md
+++ readme.md
@@ -22,15 +22,15 @@ Also available as a [gulp](https://github.com/sindresorhus/gulp-strip-json-comme
 
 ## Install
 
-```sh
+```
 $ npm install --save strip-json-comments
 ```
 
-```sh
+```
 $ bower install --save strip-json-comments
 ```
 
-```sh
+```
 $ component install sindresorhus/strip-json-comments
 ```
 
@@ -57,16 +57,19 @@ Accepts a string with JSON and returns a string without comments.
 
 ## CLI
 
-```sh
+```
 $ npm install --global strip-json-comments
 ```
 
-```sh
+```
 $ strip-json-comments --help
 
-strip-json-comments input-file > output-file
-# or
-strip-json-comments < input-file > output-file
+  Usage
+    $ strip-json-comments <input-file> > <output-file>
+    $ strip-json-comments < <input-file> > <output-file>
+
+  Example
+    $ strip-json-comments package.json > package-stripped.json
 ```
 
 
