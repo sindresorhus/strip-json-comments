0195077a87aa0db135f23a92b05e85ac6a2549da
Blake Embrey <hello@blakeembrey.com>
2015-11-05 09:31:05 -0800

    Replace comments with whitespace by default

--- test.js
+++ test.js
@@ -3,14 +3,26 @@ var assert = require('assert');
 var strip = require('./strip-json-comments');
 
 suite('Test Cases', function () {
-	test('should strip comments', function () {
-		assert.strictEqual(strip('//comment\n{"a":"b"}'), '\n{"a":"b"}');
-		assert.strictEqual(strip('/*//comment*/{"a":"b"}'), '{"a":"b"}');
-		assert.strictEqual(strip('{"a":"b"//comment\n}'), '{"a":"b"\n}');
-		assert.strictEqual(strip('{"a":"b"/*comment*/}'), '{"a":"b"}');
-		assert.strictEqual(strip('{"a"/*\n\n\ncomment\r\n*/:"b"}'), '{"a":"b"}');
-		assert.strictEqual(strip('/*!\n * comment\n */\n{"a":"b"}'), '\n{"a":"b"}');
-		assert.strictEqual(strip('{/*comment*/"a":"b"}'), '{"a":"b"}');
+	test('should replace comments with whitespace', function () {
+		assert.strictEqual(strip('//comment\n{"a":"b"}'), '         \n{"a":"b"}');
+		assert.strictEqual(strip('/*//comment*/{"a":"b"}'), '             {"a":"b"}');
+		assert.strictEqual(strip('{"a":"b"//comment\n}'), '{"a":"b"         \n}');
+		assert.strictEqual(strip('{"a":"b"/*comment*/}'), '{"a":"b"           }');
+		assert.strictEqual(strip('{"a"/*\n\n\ncomment\r\n*/:"b"}'), '{"a"  \n\n\n       \r\n  :"b"}');
+		assert.strictEqual(strip('/*!\n * comment\n */\n{"a":"b"}'), '   \n          \n   \n{"a":"b"}');
+		assert.strictEqual(strip('{/*comment*/"a":"b"}'), '{           "a":"b"}');
+	});
+
+	test('should remove comments', function () {
+		var options = { whitespace: false };
+
+		assert.strictEqual(strip('//comment\n{"a":"b"}', options), '\n{"a":"b"}');
+		assert.strictEqual(strip('/*//comment*/{"a":"b"}', options), '{"a":"b"}');
+		assert.strictEqual(strip('{"a":"b"//comment\n}', options), '{"a":"b"\n}');
+		assert.strictEqual(strip('{"a":"b"/*comment*/}', options), '{"a":"b"}');
+		assert.strictEqual(strip('{"a"/*\n\n\ncomment\r\n*/:"b"}', options), '{"a":"b"}');
+		assert.strictEqual(strip('/*!\n * comment\n */\n{"a":"b"}', options), '\n{"a":"b"}');
+		assert.strictEqual(strip('{/*comment*/"a":"b"}', options), '{"a":"b"}');
 	});
 
 	test('should not strip comments inside strings', function () {
@@ -32,18 +44,18 @@ suite('Test Cases', function () {
 		});
 
 		test('single line comment', function () {
-			assert.strictEqual(strip('{"a":"b"//c\n}'), '{"a":"b"\n}');
-			assert.strictEqual(strip('{"a":"b"//c\r\n}'), '{"a":"b"\r\n}');
+			assert.strictEqual(strip('{"a":"b"//c\n}'), '{"a":"b"   \n}');
+			assert.strictEqual(strip('{"a":"b"//c\r\n}'), '{"a":"b"   \r\n}');
 		});
 
 		test('single line block comment', function () {
-			assert.strictEqual(strip('{"a":"b"/*c*/\n}'), '{"a":"b"\n}');
-			assert.strictEqual(strip('{"a":"b"/*c*/\r\n}'), '{"a":"b"\r\n}');
+			assert.strictEqual(strip('{"a":"b"/*c*/\n}'), '{"a":"b"     \n}');
+			assert.strictEqual(strip('{"a":"b"/*c*/\r\n}'), '{"a":"b"     \r\n}');
 		});
 
 		test('multi line block comment', function () {
-			assert.strictEqual(strip('{"a":"b",/*c\nc2*/"x":"y"\n}'), '{"a":"b","x":"y"\n}');
-			assert.strictEqual(strip('{"a":"b",/*c\r\nc2*/"x":"y"\r\n}'), '{"a":"b","x":"y"\r\n}');
+			assert.strictEqual(strip('{"a":"b",/*c\nc2*/"x":"y"\n}'), '{"a":"b",   \n    "x":"y"\n}');
+			assert.strictEqual(strip('{"a":"b",/*c\r\nc2*/"x":"y"\r\n}'), '{"a":"b",   \r\n    "x":"y"\r\n}');
 		});
 	});
 });
