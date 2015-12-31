a77a3ac8a394cf433abecb8fc4ef7e5638951df7
Sindre Sorhus <sindresorhus@gmail.com>
2015-12-31 19:23:24 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -9,7 +9,6 @@ test('replace comments with whitespace', t => {
 	t.is(fn('{"a"/*\n\n\ncomment\r\n*/:"b"}'), '{"a"  \n\n\n       \r\n  :"b"}');
 	t.is(fn('/*!\n * comment\n */\n{"a":"b"}'), '   \n          \n   \n{"a":"b"}');
 	t.is(fn('{/*comment*/"a":"b"}'), '{           "a":"b"}');
-	t.end();
 });
 
 test('remove comments', t => {
@@ -21,7 +20,6 @@ test('remove comments', t => {
 	t.is(fn('{"a"/*\n\n\ncomment\r\n*/:"b"}', opts), '{"a":"b"}');
 	t.is(fn('/*!\n * comment\n */\n{"a":"b"}', opts), '\n{"a":"b"}');
 	t.is(fn('{/*comment*/"a":"b"}', opts), '{"a":"b"}');
-	t.end();
 });
 
 test('doesn\'t strip comments inside strings', t => {
@@ -29,35 +27,29 @@ test('doesn\'t strip comments inside strings', t => {
 	t.is(fn('{"a":"b/*c*/"}'), '{"a":"b/*c*/"}');
 	t.is(fn('{"/*a":"b"}'), '{"/*a":"b"}');
 	t.is(fn('{"\\"/*a":"b"}'), '{"\\"/*a":"b"}');
-	t.end();
 });
 
 test('consider escaped slashes when checking for escaped string quote', t => {
 	t.is(fn('{"\\\\":"https://foobar.com"}'), '{"\\\\":"https://foobar.com"}');
 	t.is(fn('{"foo\\\"":"https://foobar.com"}'), '{"foo\\\"":"https://foobar.com"}');
-	t.end();
 });
 
 test('line endings - no comments', t => {
 	t.is(fn('{"a":"b"\n}'), '{"a":"b"\n}');
 	t.is(fn('{"a":"b"\r\n}'), '{"a":"b"\r\n}');
-	t.end();
 });
 
 test('line endings - single line comment', t => {
 	t.is(fn('{"a":"b"//c\n}'), '{"a":"b"   \n}');
 	t.is(fn('{"a":"b"//c\r\n}'), '{"a":"b"   \r\n}');
-	t.end();
 });
 
 test('line endings - single line block comment', t => {
 	t.is(fn('{"a":"b"/*c*/\n}'), '{"a":"b"     \n}');
 	t.is(fn('{"a":"b"/*c*/\r\n}'), '{"a":"b"     \r\n}');
-	t.end();
 });
 
 test('line endings - multi line block comment', t => {
 	t.is(fn('{"a":"b",/*c\nc2*/"x":"y"\n}'), '{"a":"b",   \n    "x":"y"\n}');
 	t.is(fn('{"a":"b",/*c\r\nc2*/"x":"y"\r\n}'), '{"a":"b",   \r\n    "x":"y"\r\n}');
-	t.end();
 });
