367d7922f161d0f26e29bebca415691aa1ed77e8
coldfox <branco_tico@hotmail.com>
2017-05-19 19:53:53 +0100

    Benchmark #12
    Replace old benchmark code by bench from matcha;
    Added sample JSON file;
    Updated package.json to support bench command.

--- benchmark.js
+++ benchmark.js
@@ -1,10 +1,9 @@
-module.exports = function (fn, print) {
-	const _initial = process.hrtime();
-	fn();
-	const _final = process.hrtime(_initial);
-	if (print === true) {
-		console.log('Result: ' + _final[0] + 's ' + _final[1] + 'ns');
-	} else {
-		return _final;
-	}
-};
+'use strict';
+const fs = require('fs');
+const stripJsonComments = require('./');
+
+const json = fs.readFileSync('sample.json', 'utf8');
+
+bench('strip JSON comments', function () {
+	stripJsonComments(json);
+});
