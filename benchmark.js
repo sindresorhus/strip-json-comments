6fccd729ef1a0283289292fa2d15bbf1754940a8
slauyama <simonlauyamauchi@gmail.com>
2017-08-05 04:36:48 -0700

    Add more benchmarks (#33)

--- benchmark.js
+++ benchmark.js
@@ -4,7 +4,19 @@ const fs = require('fs');
 const stripJsonComments = require('.');
 
 const json = fs.readFileSync('sample.json', 'utf8');
+const bigJson = fs.readFileSync('sampleBig.json', 'utf8');
 
 bench('strip JSON comments', () => {
+  set('type', 'static');
 	stripJsonComments(json);
+});
+
+bench('strip JSON comments without whitespace', () => {
+	stripJsonComments(json, {
+    whitespace: false
+  });
+});
+
+bench('strip Big JSON comments', () => {
+	stripJsonComments(bigJson);
 });
